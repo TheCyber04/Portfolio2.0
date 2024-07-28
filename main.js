e41d644c3bc9bb5b0a6a8ffdbd796e2aaf16bd0c
TheCyber04 <dame3.ndoye@ucad.edu.sn>
2024-07-28 20:27:05 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text",{
-    strings:["Cybersecurity Beginner,Frontend Developer", "Web Developer"],
+    strings:["Cybersecurity Beginner","Frontend Developer", "Web Developer"],
    typeSpeed:100,
     backSpeed:100,
     BackDelay:1000,
